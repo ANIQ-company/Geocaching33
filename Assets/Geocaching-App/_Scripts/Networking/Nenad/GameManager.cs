32d3fa35b3667f0db99fd0f3650b71c938999b92
FlamboyantEntertainment <50521300+FlamboyantEntertainment@users.noreply.github.com>
2022-05-31 18:02:20 +0200

    networking
    u folderu scene folder nenad te tri scene

---
+++ Assets/Geocaching-App/_Scripts/Networking/Nenad/GameManager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public int sceneToLoad;
+    public int ID;
+
+    PhotonView view;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(1))
+        {
+            LoadMainScene();
+        }
+        else
+        {
+            view = PhotonView.Get(this);
+            if (!view.IsMine)
+            {
+                InstantiatePlayerMarker();
+            }
+            else InstantiatePlayerMarker();
+        }
+    }
+
+    private void InstantiatePlayerMarker()
+    {
+        PhotonNetwork.Instantiate("PlayerMarker", Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }   
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    void LoadMainScene()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);             //player count je tu prakticki bezveze ak netreba il smeta javi pa cu ga maknut
+        PhotonNetwork.LoadLevel("MainScene Nenad");                                                               //pazi ovdje je na ime scene ako ces renamat!!
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // nicknameove ces vjerovatno povuc iz username ako je tak javi da to povezem
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName);
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+        }
+    }
+}
---
+++ Assets/Geocaching-App/_Scripts/Networking/Nenad/Launcher.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 40; //??
+    string gameVersion = "1";
+
+    bool isConnecting;
+    
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+
+        }
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+        Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        isConnecting=false;
+        Destroy(this.gameObject);
+    }
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        PhotonNetwork.LoadLevel("Loading Nenad");
+    }
+}
